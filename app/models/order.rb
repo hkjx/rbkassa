25cd84cb771f5ce8cf317ed59c2bee2eb7a370f6
hkjx <humanresourcett@gmail.com>
2012-05-30 15:27:06 +0300

    'first'

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,16 @@
 class Order < ActiveRecord::Base
   attr_accessible :currency, :price
+
+  def get_pay
+  	if self.merchant == "Robokassa"
+  		merchant_url = Robokassa::MERCHANT_URL
+  		login = Robokassa::MERCHANT_LOGIN
+  		pass = Robokassa::MERCHANT_PASS_1
+		  price = Robokassa::get_rates(self.currency,self.price)
+	    sign = Digest::MD5.hexdigest([login,price, 1.to_s, pass, "shpa=#{self.currency}"].join(':'))
+
+		req_url = "#{merchant_url}?MrchLogin=#{login}&OutSum=#{price}&InvId=1&SignatureValue=#{sign}&IncCurrLabel=#{self.currency}&Culture=ru&shpa=#{self.currency}"
+  	end
+  end 
+  
 end
---
+++ app/models/robokassa.rb
@@ -0,0 +1,44 @@
+require 'open-uri'
+
+class Robokassa
+
+  MERCHANT_URL = 'http://test.robokassa.ru/Index.aspx'
+  SERVICES_URL = 'http://test.robokassa.ru/Webservice/Service.asmx'
+  MERCHANT_LOGIN = 'hkjx'
+  MERCHANT_PASS_1 = 'asdf1234'
+  MERCHANT_PASS_2 = 'zxcv0987'
+
+  def self.get_currencies(webmoney = true, lang = "ru")
+  	req_url = "#{SERVICES_URL}/GetCurrencies?MerchantLogin=#{MERCHANT_LOGIN}&Language=#{lang}"
+  	doc = Nokogiri::XML(open(req_url))
+
+  	if doc.xpath("//xmlns:Result/xmlns:Code").text.to_i == 0 
+
+	       result = doc.xpath("//*[@Code='EMoney']//xmlns:Currency").map do |c|
+	      		c = [c["Name"], c["Label"]]
+	       end
+
+	       result += doc.xpath("//*[@Code='BankCard']//xmlns:Currency").map do |c|
+	      		c = [c["Name"], c["Label"]]	   
+	      	end 	
+	end
+  end
+
+  def self.get_payment_methods(lang = "ru")
+  	req_url = "#{SERVICES_URL}/GetPaymentMethods?MerchantLogin=#{MERCHANT_LOGIN}&Language=#{lang}"
+  	doc = Nokogiri::XML(open(req_url))
+  	if doc.xpath("//xmlns:Result/xmlns:Code").text.to_i == 0
+      doc.xpath("//xmlns:Method").map do |m|
+        {:code => m["Code"], :desc => m["Description"]}
+      end
+  	end
+  end
+
+  def self.get_rates (currency, out_summ, lang = "ru")
+  	req_url="http://test.robokassa.ru/Webservice/Service.asmx/GetRates?MerchantLogin=hkjx&IncCurrLabel=#{currency}&OutSum=#{out_summ}&Language=ru"
+  	doc = Nokogiri::XML(open(req_url))
+  	if doc.xpath("//xmlns:Result/xmlns:Code").text.to_i == 0
+		doc.xpath("//*[@Label='#{currency}']//xmlns:Rate").first["IncSum"].to_f
+  	end
+  end 
+end
