6c24a2f462176b66fc1ef6453ff33d4f9e92845e
hkjx <humanresourcett@gmail.com>
2012-05-30 16:42:29 +0300

    'first7'

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
-  CURRENTIES = Robokassa.get_currencies
+  #CURRENTIES = Robokassa.get_currencies
   attr_accessible :currency, :price
   attr_reader :currenties
 
@@ -7,10 +7,10 @@ class Order < ActiveRecord::Base
   		merchant_url = Robokassa::MERCHANT_URL
   		login = Robokassa::MERCHANT_LOGIN
   		pass = Robokassa::MERCHANT_PASS_1
-		  price = Robokassa::get_rates(self.currency,self.price)
-	    sign = Digest::MD5.hexdigest([login,price, 1.to_s, pass, "shpa=#{self.currency}"].join(':'))
+		  #price = Robokassa::get_rates(self.currency,self.price)
+	    sign = Digest::MD5.hexdigest([login,self.price, 1.to_s, pass].join(':'))
 
-		req_url = "#{merchant_url}?MrchLogin=#{login}&OutSum=#{price}&InvId=1&SignatureValue=#{sign}&IncCurrLabel=#{self.currency}&Culture=ru&shpa=#{self.currency}"
+		req_url = "#{merchant_url}?MrchLogin=#{login}&OutSum=#{price}&InvId=1&SignatureValue=#{sign}&IncCurrLabel=#{self.currency}&Culture=ru"
 
   end 
   
