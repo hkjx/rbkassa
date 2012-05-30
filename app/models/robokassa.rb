285747bd90dbfee218d6cee6a61aa811b9b97690
hkjx <humanresourcett@gmail.com>
2012-05-30 16:22:49 +0300

    'first5'

--- app/models/robokassa.rb
+++ app/models/robokassa.rb
@@ -12,14 +12,14 @@ class Robokassa
   def self.get_currencies(webmoney = true, lang = "ru")
   	req_url = "#{SERVICES_URL}/GetCurrencies?MerchantLogin=#{MERCHANT_LOGIN}&Language=#{lang}"
   	doc = Nokogiri::XML(open(req_url))
-    doc.remove_namespaces!
 
-  	if doc.xpath("//Result/Code").text.to_i == 0 
-	       result = doc.xpath("//*[@Code='EMoney']//Currency").map do |c|
+  	if doc.xpath("//xmlns:Result/xmlns:Code").text.to_i == 0 
+
+	       result = doc.xpath("//*[@Code='EMoney']//xmlns:Currency").map do |c|
 	      		c = [c["Name"], c["Label"]]
 	       end
 
-	        result += doc.xpath("//*[@Code='BankCard']//Currency").map do |c|
+	        result += doc.xpath("//*[@Code='BankCard']//xmlns:Currency").map do |c|
 	      	 	c = [c["Name"], c["Label"]]	   
 	      	 end 	
 	end
@@ -38,9 +38,8 @@ class Robokassa
   def self.get_rates (currency, out_summ, lang = "ru")
   	req_url="http://test.robokassa.ru/Webservice/Service.asmx/GetRates?MerchantLogin=hkjx&IncCurrLabel=#{currency}&OutSum=#{out_summ}&Language=ru"
   	doc = Nokogiri::XML(open(req_url))
-    doc.remove_namespaces!
-  	if doc.xpath("//Result/Code").text.to_i == 0
-		doc.xpath("//*[@Label='#{currency}']//Rate").first["IncSum"].to_f
+  	if doc.xpath("//xmlns:Result/xmlns:Code").text.to_i == 0
+		doc.xpath("//*[@Label='#{currency}']//xmlns:Rate").first["IncSum"].to_f
   	end
   end 
 end
